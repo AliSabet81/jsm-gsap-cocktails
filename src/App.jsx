c4456e7feb46c5975d94406083afd295b0acf27f
AliSabet81 <sabet.farshid1381@gmail.com>
2025-06-25 13:53:29 +0330

    art section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Art from "./components/Art.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
